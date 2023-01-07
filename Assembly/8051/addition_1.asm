e20f597ea1dc81dcb03f4006d5b0a8d0936e6e64
pavan <awsdevops.pavan@gmail.com>
2023-01-07 10:02:29 +0530

    Added addition program and updated readme file

--- Assembly/8051/addition_1.asm
+++ Assembly/8051/addition_1.asm
@@ -1,2 +1,10 @@
 ; WAP to add the contents of Internal RAM locations 40H and 41H. Store
 ; Result at 42H and Carry at 43H
+
+        MOV 43H, #00H ; Initialize Carry as “0”
+        MOV A, 40H ; Read first number
+        ADD A, 41H ; Add second number
+        JNC SKIP ; If no Carry, directly store the Sum
+        INC 43H ; Store Carry as “1”
+SKIP:   MOV 42H, A ; Store Sum
+HERE:   SJMP HERE ; End of program
